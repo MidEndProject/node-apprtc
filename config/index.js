d9d424361c7d7081a9e52a6dd913632c0b330770
Fitra Aditya <fitra@g.pl>
2016-01-29 13:52:12 +0700

    Change TURN_BASE_URL and WSS_HOST.

--- config/index.js
+++ config/index.js
@@ -1,9 +1,9 @@
 module.exports = {
   constant: {
     LOOPBACK_CLIENT_ID: 'LOOPBACK_CLIENT_ID',
-    TURN_BASE_URL: 'http://demo6614221.mockable.io',
+    TURN_BASE_URL: 'https://node-apprtc.herokuapp.com',
     TURN_URL_TEMPLATE: '%s/turn?username=%s&key=%s',
-    WSS_HOST_PORT_PAIRS: ['halodoc-webrtc.linkdokter.com:8443'],
+    WSS_HOST_PORT_PAIRS: ['node-apprtc-ws.herokuapp.com'],
     RESPONSE_UNKNOWN_ROOM: 'UNKNOWN_ROOM',
     RESPONSE_UNKNOWN_CLIENT: 'UNKNOWN_CLIENT',
     RESPONSE_ROOM_FULL: 'FULL',
