289ce990227bd3eadb3091bd5998f2d0c206053d
Fitra Aditya <fitra@g.pl>
2016-01-29 13:52:38 +0700

    Add route for /turn request.

--- controller/index.js
+++ controller/index.js
@@ -4,6 +4,24 @@ exports.main = {
   handler: function (request, reply) {
     var params = Common.getRoomParameters(request, null, null, null);
 
-    return reply.view('index_template', params);
+    reply.view('index_template', params);
+  }
+};
+
+exports.turn = {
+  handler: function (request, reply) {
+    reply({
+      "username":"webrtc",
+      "password":"webrtc",
+      "uris":[
+        "stun:stun.l.google.com:19302",
+        "stun:stun1.l.google.com:19302",
+        "stun:stun2.l.google.com:19302",
+        "stun:stun3.l.google.com:19302",
+        "stun:stun4.l.google.com:19302",
+        "stun:stun.services.mozilla.com",
+        "turn:turn.anyfirewall.com:443?transport=tcp"
+      ]
+    });
   }
 };
