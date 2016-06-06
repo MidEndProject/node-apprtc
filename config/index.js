7f4336c640bf77a1b67262fd9526deafad77073c
Fitra Aditya <fitra@g.pl>
2016-06-06 13:50:45 +0700

    Remove credentials path in TURN request URL.

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   constant: {
     LOOPBACK_CLIENT_ID: 'LOOPBACK_CLIENT_ID',
     TURN_BASE_URL: 'https://node-apprtc.herokuapp.com',
-    TURN_URL_TEMPLATE: '%s/turn?username=%s&key=%s',
+    TURN_URL_TEMPLATE: '%s/turn',
     WSS_HOST_PORT_PAIRS: ['node-apprtc-ws.herokuapp.com'],
     RESPONSE_UNKNOWN_ROOM: 'UNKNOWN_ROOM',
     RESPONSE_UNKNOWN_CLIENT: 'UNKNOWN_CLIENT',
